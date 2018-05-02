52ace8e0a0289b03b51d57e801da8719efa309eb
Veronica <4597veronica@gmail.com>
2018-05-02 21:18:37 +0200

    Add files via upload

---
+++ js/javascript-fusteria.js
@@ -0,0 +1,36 @@
+        /* Para mostrar las flechas*/
+        function mostrar() {
+            var x = document.getElementById("flechas_izquierda");
+            var y = document.getElementById("flechas_abajo");
+            
+            //Flechas que van hacia un lado
+            if (x.style.display === "block") {
+                x.style.display = "none";
+            } else {
+                x.style.display = "block";
+            }
+            //Flechas que apuntan hacia arriba
+            if (y.style.display === "block") {
+                y.style.display = "none";
+            } else {
+                y.style.display = "block";
+            }
+        }
+        /* Para que el menu sea desplegable*/
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+        }
+
+        window.onclick = function(e) {
+          if (!e.target.matches('.dropbtn')) {
+            var myDropdown = document.getElementById("myDropdown");
+              if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+              }
+          }
+        }
+			/*ocultar flechas*/
+			function verform(){
+ 				document.getElementById("flechas_izquierda").style.visibility="hidden";
+ 				document.getElementById("flechas_abajo").style.visibility="hidden";
+			} 
